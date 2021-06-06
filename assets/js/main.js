21701e16926bbc747e530474138494f89c90877d
brian_kulei <briankulei98@gmail.com>
2021-06-06 18:02:07 +0300

    add bootstrap file

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+
+(function() {
+  "use strict";
+
+ 
+
+  /**
+   * Testimonials slider
+   */
+  new Swiper('.testimonials-slider', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
+
+
+ 
+
+})()
