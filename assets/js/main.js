988ac3d054a1abd58c6f77871539f490f5dbce84
brian_kulei <briankulei98@gmail.com>
2021-06-08 13:05:26 +0300

    add my email in index.html file

--- assets/js/main.js
+++ assets/js/main.js
@@ -95,7 +95,7 @@
   let backtotop = select('.back-to-top')
   if (backtotop) {
     const toggleBacktotop = () => {
-      if (window.scrollY > 100) {
+      if (window.scrollY > 500) {
         backtotop.classList.add('active')
       } else {
         backtotop.classList.remove('active')
